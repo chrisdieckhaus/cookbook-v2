b2f053e3e30ff0c43a2bb06852a4effe47065091
Will Victor <will.victor10@gmail.com>
2019-09-30 21:42:35 -0400

    Set up deployment pipeline to gcloud through circle ci

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, Cookbook!'));
+app.get('/', (req, res) => res.send('Hello, Cookbook-v2!'));
 
 app.listen(port, () => console.log(`Cookbook listening on port ${port}!`));
