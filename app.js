902dc598df74aa12c0352f1643401feecb131c0e
Chris <christopherdieckhaus@gmail.com>
2019-10-21 20:51:42 -0400

    Hello world for graphql

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, Cookbook-v2!'));
+var schema = buildSchema(`
+  type Query {
+    hello: String,
+    Will: String
+  }
+`);
 
+var root = { 
+    hello: () => 'Hello cookbook! This is GraphQL.' ,
+    Will: "Will is a guy"
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.get('/', (req, res) => res.send('Hello, Cookbook-v2! Check for graphql at /graphql'));
 app.listen(port, () => console.log(`Cookbook listening on port ${port}!`));
